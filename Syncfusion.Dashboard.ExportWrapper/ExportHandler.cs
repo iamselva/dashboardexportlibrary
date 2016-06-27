188e9e4cb361b486fd15b11b86fc3fd7aa7a909a
gangatharananandavel <gangatharananandavel@syncfusion.com>
2016-06-27 04:35:22 -0400

    Wrapper class library added for handling export operations
    Wrapper class library added for handling export operations

--- Syncfusion.Dashboard.ExportWrapper/ExportHandler.cs
+++ Syncfusion.Dashboard.ExportWrapper/ExportHandler.cs
@@ -6,14 +6,14 @@ namespace Syncfusion.Dashboard.ExportWrapper
     {
         public void ExportUsingPhantom(string token)
         {
-            var p = new System.Diagnostics.Process();
+            using (var p = new System.Diagnostics.Process())
             {
                 Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
                 p.StartInfo.FileName = "phantomjs.exe";
                 p.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
                 p.StartInfo.Arguments = token + "js.js  --disk-cache=[true]";
                 p.Start();
-                p.WaitForExit();
+                p.WaitForExit(300000);
             }
         }           
     }
